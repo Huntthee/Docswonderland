42d5f4a536ee30949a9969e934cf2375e570db70
Huntthee <mikeallison000@gmail.com>
2021-08-22 12:15:49 -0600

    mobile image not showing on production

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,7 @@ class Hero extends React.Component {
   render() {
     return (
       <section id="hero-section">
-        <img src="./rust-logo.svg" alt="Logo for the survival game, Rust." id="mobile-logo"/>
+        <img src="../Images/rust-logo.svg" alt="Logo for the survival game, Rust." id="mobile-logo"/>
         <h2>A lightly modded Rust server</h2>
         <a href="https://discord.gg/wVAz49ruAf" target="_blank" rel="noreferrer">Join the Discord</a>
       </section>
