d79b934410727a81aa1ed59937468b1b059b70cd
Huntthee <mikeallison000@gmail.com>
2021-09-06 13:23:20 -0600

    site updates

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -6,18 +6,18 @@ class Events extends React.Component {
     return (
       <section id="events-section">
         <article>
-          <h2>Upcoming Events</h2>
+          <h2>Upcoming & Current Events</h2>
         </article>
         <article>
           <h3>Doc's Wonderland Returns!</h3>
-          <p>LIVE Sept 3 2021</p>
+          <p>Server LIVE Now!</p>
         </article>
         <article>
-          <h3>Duelist mode</h3>
-          <p>Coming this weekend, face off 1v1 against your rivals with this Rust mod.</p>
+          <h3>Duelist Mode Introduced</h3>
+          <p>Face off 1v1 against your rivals with this Rust mod.</p>
         </article>
         <article>
-          <h3>No Events</h3>
+          <h3>No Events Yet!</h3>
           <p>Upcoming events will appear here with a short description of the festivities to enjoy.</p>
         </article>
       </section>
